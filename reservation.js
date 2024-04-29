f968797da4b1f32f040b46d5da0f6f0e9f26a11c
Felix Veras <fcarvajal44@gmail.com>
2024-04-29 12:13:54 -0400

    Add code

---
+++ reservation.js
@@ -0,0 +1,34 @@
+var tableList = [
+    { id: 1, seat: 4 },
+    { id: 2, seat: 6 },
+    { id: 3, seat: 2 },
+    { id: 4, seat: 8 },
+    { id: 5, seat: 4 },
+    { id: 6, seat: 4 },
+    { id: 7, seat: 4 },
+    { id: 8, seat: 4 },
+    { id: 9, seat: 10 },
+    { id: 10, seat: 1 },
+    { id: 11, seat: 3 },
+    { id: 12, seat: 4 },
+    { id: 13, seat: 4 },
+    { id: 14, seat: 2 },
+    { id: 15, seat: 4 },
+    { id: 16, seat: 4 },
+    { id: 17, seat: 5 },
+    { id: 18, seat: 3 },
+    { id: 19, seat: 4 },
+];
+var maxCapacityList = 120;
+var totalReservedSeats = tableList.reduce(function (total, table) { return total + table.seat; }, 0);
+function getTable(availableNumberSeats) {
+    var remainingSeats = maxCapacityList - totalReservedSeats;
+    if (availableNumberSeats !== null && availableNumberSeats <= remainingSeats) {
+        return "Here's some space. ".concat(remainingSeats, " remaining seats.");
+    }
+    else {
+        return "Sold out";
+    }
+}
+console.log(getTable(56));
+console.log(getTable(28));
